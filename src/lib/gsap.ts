c86b809ff5be35b558b9d1469d609899683b34af
Sultan <72926764+sultan-O7t9@users.noreply.github.com>
2026-07-29 16:20:59 +0500

    Adding Home Products Slider

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -1,8 +1,9 @@
 // src/lib/gsap.ts
-import { gsap } from 'gsap';
-import { SplitText } from 'gsap/SplitText';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 
-gsap.registerPlugin(SplitText, ScrollTrigger);
+gsap.registerPlugin(SplitText, ScrollTrigger, ScrambleTextPlugin);
 
 export { gsap, SplitText, ScrollTrigger };
