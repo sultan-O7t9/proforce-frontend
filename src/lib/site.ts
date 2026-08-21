162025a2a3c27ef4313f281ca91909d1f2be9636
Sultan <72926764+sultan-O7t9@users.noreply.github.com>
2026-08-21 12:27:29 +0500

    Update site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -42,7 +42,7 @@ export const NAV_LINKS = [
 ] as const;
 
 export const FOOTER_LINKS = [
-  { id: 1, name: "OneFilm USA", href: "" },
+  { id: 1, name: "OneFilmUSA", href: "" },
   { ...ROUTES.PRIVACY },
   { ...ROUTES.TERMS },
 ];
